77dac12b04ac3c05798ad3a394ad3778b23ea123
Patrick <g00409592@atu.ie>
2023-01-09 18:01:06 +0000

    Empty com

---
+++ src/main/java/ie/atu/week8/Main.java
@@ -0,0 +1,13 @@
+package ie.atu.week8;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+
+    }
+}
